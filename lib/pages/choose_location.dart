19b0ea653264412f376bea6865f6307f1c086db9
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-05 16:55:49 +0530

    add http dependencies

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,17 +6,31 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    //simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return "Achi";
+    });
+
+    //simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return "Darshana Senevirathna";
+    });
+
+    print("$username - $bio ");
+  }
+
   int counter = 0;
 
   @override
   void initState() {
     super.initState();
-    print("Initstate function ran");
+    getData();
+    print("hey! there");
   }
 
   @override
   Widget build(BuildContext context) {
-    print("build function ran");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -25,14 +39,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
-        child: Text("Counter is $counter"),
-      ),
+//
     );
   }
 }
