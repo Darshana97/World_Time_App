38e9c65ddc7a33a00e0a4385d09784d574ba7039
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-06 13:09:16 +0530

    add loading spinners

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time_app/sevices/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -29,10 +30,14 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
-      ),
-    );
+        backgroundColor: Colors.blue[900],
+        body: Center(
+          child: Center(
+            child: SpinKitDoubleBounce(
+              color: Colors.white,
+              size: 80.0,
+            ),
+          ),
+        ));
   }
 }
